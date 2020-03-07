22729d03119b87a27461788903ac99d6b3f593c3
c00kamunga <tsothalanti2@yahoo.com>
2020-03-06 17:12:29 -0800

    added gender and height to character traits

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -4,7 +4,11 @@ function CharacterList(props) {
     return (
         <div>
             {props.people.map(swPeople => (
+                <>
                 <h1>Name:{swPeople.name}</h1>
+            <h3>Gender{swPeople.gender}</h3>
+            <h3>Weight{swPeople.height}</h3>
+            </>
             ))}
         </div>
     )
