aa21219596cd9fd8ef25f674c27aff54770fc56f
c00kamunga <tsothalanti2@yahoo.com>
2020-03-06 16:28:09 -0800

    installed axios and styled components, imported axios, useState, and useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
     <div className="App">
@@ -17,3 +12,17 @@ const App = () => {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
